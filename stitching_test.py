b55aa8fe2668d2c33c394528a4e7ce91449f5cb8
Ulrixon <61776179+Ulrixon@users.noreply.github.com>
2023-05-12 01:18:58 +0800

    up

--- stitching_test.py
+++ stitching_test.py
@@ -13,9 +13,9 @@ def find_tetragon(img):
 # threshold image
 
     ret,thresh = cv2.threshold(img*255,127,255,0)
-    #cv2.imshow('threshold ',thresh)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.imshow('threshold ',thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     #print(thresh.shape)
 # dilate thresholded image - merges top/bottom 
     #kernel = np.ones((3,3), np.uint8)
@@ -26,8 +26,8 @@ def find_tetragon(img):
     contours, hierarchy = cv2.findContours(thresh.astype(np.uint8),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 # cv2.drawContours(img, contours, 0, (255,255,255), 3)
     #print(contours)
-    print ("contours:",len(contours))
-    print ("largest contour has ",len(contours[0]),"points")
+    #print ("contours:",len(contours))
+    #print ("largest contour has ",len(contours[0]),"points")
 
 # minAreaRect
 # rect = cv2.minAreaRect(contours[0])
@@ -41,10 +41,21 @@ def find_tetragon(img):
 # print "convex hull has ",len(hull),"points"
 
 # simplify contours
-    epsilon = 0.1*cv2.arcLength(contours[0],True)
+    epsilon = 0.01*cv2.arcLength(contours[0],True)
     approx = cv2.approxPolyDP(contours[0],epsilon,True)
-    cv2.drawContours(img, [approx], 0, (125), 3)
+    #cv2.drawContours(img, [approx], 0, (125), 3)
     print ("simplified contour has",len(approx),"points")
+    if(len(approx)<4):
+        print("not tetragon and not enough point")
+        epsilon = 0.001*cv2.arcLength(contours[0],True)
+        approx = cv2.approxPolyDP(contours[0],epsilon,True)
+    elif(len(approx)>4):
+        print("not tetragon and too much point")
+        epsilon = 0.5*cv2.arcLength(contours[0],True)
+        approx = cv2.approxPolyDP(contours[0],epsilon,True)
+
+
+    
     print(approx)
 
 
@@ -60,17 +71,20 @@ def find_tetragon(img):
 def create_mask(gray): #mask to remove black background
 # threshold
     thresh = cv2.threshold(gray, 11, 255, cv2.THRESH_BINARY)[1]
+    #cv2.imshow('image',thresh)
 
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 # apply morphology to clean small spots
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-    morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-    morph = cv2.morphologyEx(morph, cv2.MORPH_ERODE, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
+    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    #morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
+    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    #morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
+    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    #morph = cv2.morphologyEx(morph, cv2.MORPH_ERODE, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
 
 # get external contour
-    contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
     big_contour = max(contours, key=cv2.contourArea)
 
@@ -275,24 +289,69 @@ def stitch_and_blend(img1,img2,algorithm="SIFT"):
 #%%
 # Load the images
 
+import os
+from os import listdir
+from os.path import isfile, join
 
 
 
-img1 = cv2.imread("/mnt/c/Users/ryan7/Downloads/test_samples-20230511T131513Z-001/test_samples/left/001.jpg")
-img2 = cv2.imread("/mnt/c/Users/ryan7/Downloads/test_samples-20230511T131513Z-001/test_samples/right/001.jpg")
+label_folder = []
+total_size = 0
+data_path = "/mnt/c/Users/ryan7/Downloads/test_samples-20230511T131513Z-001/test_samples"
+
+
+
+for root, dirts, files in os.walk(data_path+r"/left"):
+    total_size += len(files)
+    for dirt in dirts:
+        label_folder.append(dirt)
+        #total_size = total_size+  len(files)
+print("found", total_size, "files.")
+print("folder:", label_folder)
+
+
+for text in files:
+    #text=text.replace(".jpg", "")
+
+    img1 = cv2.imread(data_path+ r"/left/"+text)
+    img2 = cv2.imread(data_path+ r"/right/"+text)
+    result,axis=stitch_and_blend(img1,img2,"KAZE")
+#img = Image.fromarray(result, 'RGB')
+
+
+    #cv2.drawContours(result, [axis], 0, (0,255,0), 3)
+
+#cv2.imshow("Stitched Image", result)
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+    name=text.replace(".jpg", "")
+    cv2.imwrite('sample/'+name+'.png', result*255)
+
+
+#print("found", total_size, "files.")
+#print("folder:", label_folder)
+
+#%%
+#img1 = cv2.imread("/mnt/c/Users/ryan7/Downloads/test_samples-20230511T131513Z-001/test_samples/left/001.jpg")
+#img2 = cv2.imread("/mnt/c/Users/ryan7/Downloads/test_samples-20230511T131513Z-001/test_samples/right/001.jpg")
 #%%
 
 
 
 #plt.imshow("Matching Image", result.astype('uint8'))
-result,axis=stitch_and_blend(img1,img2,"KAZE")
+#result,axis=stitch_and_blend(img1,img2,"KAZE")
 #img = Image.fromarray(result, 'RGB')
 #img.show()
+
+
+#cv2.drawContours(result, [axis], 0, (0,255,0), 3)
+
 #cv2.imshow("Stitched Image", result)
 
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-cv2.imwrite('sample1.png', result*255)
+#cv2.imwrite('sample1.png', result*255)
 
 
 # %%
